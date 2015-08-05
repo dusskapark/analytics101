e2910d8025ab2230e6d768441c0e073fe96648f9
dusskapark <dusskapark@gmail.com>
2015-08-05 19:03:09 +0900

    메뉴기능 동작하도록 수정
    메뉴기능 동작하도록 수정

--- js/init.js
+++ js/init.js
@@ -1,79 +1,143 @@
-$(document).ready(function() {
-    $('.modal-trigger').leanModal();
+(function($){
+  $(function(){
 
-    $.ajax({
-        type : 'GET',
-        url : "http://munsangdong.cafe24.com/api/card",
-        dataType : "json",
-        success : response_json
+    
+    // $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    var url = "http://munsangdong.cafe24.com/api/card";  
+    var callApi = function(url) {
+        $.ajax({
+            type : 'GET',
+            url : url,
+            dataType : "json",
+            success : response_json
+        });
+    };
+
+    callApi(url);
+
+    $("ul.category>li>a").click(function() {
+        var hash = window.location.hash;
+        hash = hash.slice(1);
+        url = 'http://munsangdong.cafe24.com/api/card?category=' + hash;
+        console.log(url);
+        //카드 목록 초기
+        $("#FeviCard").empty();
+        callApi(url);
     });
+    
+
+    // $("ul.category>li>a").click(function(e) {
+    // var category = $(e.target).attr("class");
+    // //url = 'http://munsangdong.cafe24.com/api/card?category=' + category;
+    // console.log(category);
+    //
+    // });
 
     function response_json(json) {
+        console.log(json);
 
         var video_list = json.content;
 
-        //비디오 숫자
-        var video_count = video_list.length;
+        video_list.forEach(function(v, i) {
+            var item = v;
+                        // 카드를 구성한다
+                        var card = "<div class='col s12 m12 l4 grid-item'>" +
+                            "<div class='card'>" +
+                                "<div class='card-image waves-effect waves-block waves-light'>" +
+                                    "<a href='#" + item.id + "' class='modal-trigger' /><img width='800' height='600' src=' " + item.picture + " ' class='responsive-img'/></a>" +
+                                        "<span class='card-title'>" + item.category + "<i class='material-icons'>play_circle_filled</i></span>" +
+                                "</div>" +
+                                "<div class='card-content'><span class='card-title activator grey-text text-darken-4 truncate'>" + item.name + "</span>" +
+                                    "<p>" + item.description + "</p>" + 
+                                "</div>" +
+                                "<div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + item.category + "<i class='material-icons right'>close</i></span>" +
+                                "<ul class='collection'><li class='collection-item avatar'><img src='" + item.profile_image + "' class='circle'>" +
+                                    "<span class='title'>" + item.name + "</span>" +
+                                    "<p>updated: " + item.updated_time + "</br>" +
+                                        "created: " + item.created_time + "</p></li></ul>" +
+                                "</div>" + 
+                              "</div>";
 
-        //비디오 숫자만큼 루프를 돈다.
-        $.each(video_list, function(key) {
-            // 비디오 1개의 정보를 가진 변수
-            // video_list의 배열에서 각 개체를 가져온다.
-            var info = video_list[key];
-
-            var id = info.id;
-            var source = info.source;
-            var thumbnail = info.picture;
-            var description = info.description;
-            var width = info.width;
-            var height = info.height;
-            var status = info.status;
-
-            // FB 페이지 이름을 카테고리 등 공통 정보를 구한다.
-            var FBpage_name = info.name;
-            var FBcategory = info.category;
-            var FBprofile_image = info.profile_image;
-
-            var updated_time = info.updated_time;
-            var created_time = info.created_time;
-
-            // 카드를 구성한다
-            var card = "<div class='col s12 m12 l4'>";
-            card += "<div class='card small hoverable'>";
-            card += "<div class='card-image waves-effect waves-block waves-light'>";
-            card += "<img width='800' height='600' src=' " + thumbnail + " ' class='responsive-img'/>";
-            card += "<a href='#" + id + "' class='modal-trigger' />";            
-            card += "<span class='card-title'>" + FBcategory + " <i class='large material-icons'>play_circle_filled</i></span> </a></div>";
-            card += "<div class='card-content'><span class='card-title activator grey-text text-darken-4 truncate'>" + FBpage_name + "</span>";
-            card += "<p>" + description + "</p></div>";
-            card += "<div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + FBcategory + "<i class='material-icons right'>close</i></span>";
-            card += "<ul class='collection'><li class='collection-item avatar'><img src='" + FBprofile_image + "' class='circle'>";
-            card += "<span class='title'>" + FBpage_name + "</span>";
-            card += "<p>updated: " + updated_time + "</br>";
-            card += "created: " + created_time + "</p></li></ul>";
-            card += "</div></div>";
 
             //카드를 화면에 표시한다.
             $("#FeviCard").append(card);
-            
-            
-            //비디오 플레이 modal
-            var playCard = "<div id='" + id +"' class='modal'>";
-            playCard += "<div class='modal-content'>";
-            playCard += "<h4>" + FBpage_name + "</h4>";
-            playCard += "<video class='videoPlay' controls muted autoplay poster='" + thumbnail + "'>";
-            playCard += "<source src='" + source  + "' width='" + width + "' height='" + height + "' /></video>";
-            playCard += "<p>" + description + "</p>";
-            playCard += "</div>";
-            playCard += "<div class='modal-footer'><a href='#!' class='modal-action modal-close waves-effect waves-green btn-flat'>Close</a></div></div>";
-
-            //카드를 화면에 표시한다.
-            $("#modalView").append(playCard);
-
-
         });
     };
+
 });
+// jquery
+
+// function response_json(json) {
+//
+// var video_list = json.content;
+//
+// //비디오 숫자
+// var video_count = video_list.length;
+//
+// //비디오 숫자만큼 루프를 돈다.
+// $.each(video_list, function(key) {
+// // 비디오 1개의 정보를 가진 변수
+// // video_list의 배열에서 각 개체를 가져온다.
+// var info = video_list[key];
+//
+// var id = info.id;
+// var source = info.source;
+// var thumbnail = info.picture;
+// var description = info.description;
+// var width = info.width;
+// var height = info.height;
+// var status = info.status;
+//
+// // FB 페이지 이름을 카테고리 등 공통 정보를 구한다.
+// var FBpage_name = info.name;
+// var FBcategory = info.category;
+// var FBprofile_image = info.profile_image;
+//
+// var updated_time = info.updated_time;
+// var created_time = info.created_time;
+//
+// // 카드를 구성한다
+// var card = "<div class='col s12 m12 l4'>";
+// card += "<div class='card small hoverable'>";
+// card += "<div class='card-image waves-effect waves-block waves-light'>";
+// card += "<img width='800' height='600' src=' " + thumbnail + " ' class='responsive-img'/>";
+// card += "<a href='#" + id + "' class='modal-trigger' >";
+// card += "<span class='card-title'>" + FBcategory + " <i class='large material-icons'>play_circle_filled</i></span> </a></div>";
+// card += "<div class='card-content'><span class='card-title activator grey-text text-darken-4 truncate'>" + FBpage_name + "</span>";
+// card += "<p>" + description + "</p></div>";
+// card += "<div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + FBcategory + "<i class='material-icons right'>close</i></span>";
+// card += "<ul class='collection'><li class='collection-item avatar'><img src='" + FBprofile_image + "' class='circle'>";
+// card += "<span class='title'>" + FBpage_name + "</span>";
+// card += "<p>updated: " + updated_time + "</br>";
+// card += "created: " + created_time + "</p></li></ul>";
+// card += "</div></div>";
+//
+// //카드를 화면에 표시한다.
+// $("#FeviCard").append(card);
+//
+//
+// //비디오 플레이 modal
+// var playCard = "<div id='" + id +"' class='modal'>";
+// playCard += "<div class='modal-content'>";
+// playCard += "<h4>" + FBpage_name + "</h4>";
+// playCard += "<video class='videoPlay' controls muted autoplay poster='" + thumbnail + "'>";
+// playCard += "<source src='" + source  + "' width='" + width + "' height='" + height + "' /></video>";
+// playCard += "<p>" + description + "</p>";
+// playCard += "</div>";
+// playCard += "<div class='modal-footer'><a href='#!' class='modal-action modal-close waves-effect waves-green btn-flat'>Close</a></div></div>";
+//
+// //카드를 화면에 표시한다.
+// $("#modalView").append(playCard);
+//
+//
+// });
+// };
+// });
 
 // end of document ready
 
--- js/init_xml.js
+++ js/init_xml.js
@@ -1,5 +1,9 @@
-(function($){
-  $(function(){
+// 사용할 예정인.. js 코드모음 
+
+//Tistory RSS를 받아서 카드를 구성하기.
+
+(function($) {
+	$(function() {
 		// 홈/카테고리 별 화면 인식
 		if (location.pathname == "/") {
 			// /로 구분을 한다고 상상하고..
@@ -13,63 +17,82 @@
 		}
 		// API 불러오기
 		var recent_items = [];
-		function loadViaFeviApi (n,l){
-			$.ajax({
-				url: "./sample.json",
-				dataType: "json",
-				success: function(t) {
-					$(t).find("content").each(function() {
-						var content = {};
-						var link, thumb, title, desc, date;
-						link = $(this).children("link").text();
-						link = link.replace("http://", "");
-						if (link.split("/")[2] !== undefined) link = "/" + link.split("/")[1] + "/" + link.split("/")[2];
-						else link = "/" + link.split("/")[1];
-						title = $(this).children("title").text();
-						date = $(this).children('pubDate').text();
-						desc = $(this).children("description").text();
-						if (desc.match("http://cfile(.*?)\"") !== null) {
-							thumb = desc.match("http://cfile(.*?)\"")[0];
-							thumb = thumb.substring(0, thumb.length - 1);
-						} else {
-							thumb = "";
+		function loadViaRSS(n, l) {
+			$
+					.ajax({
+						url : "./rss",
+						dataType : "xml",
+						success : function(t) {
+							$(t)
+									.find("content")
+									.each(
+											function() {
+												var content = {};
+												var link, thumb, title, desc, date;
+												link = $(this).children("link")
+														.text();
+												link = link.replace("http://",
+														"");
+												if (link.split("/")[2] !== undefined)
+													link = "/"
+															+ link.split("/")[1]
+															+ "/"
+															+ link.split("/")[2];
+												else
+													link = "/"
+															+ link.split("/")[1];
+												title = $(this).children(
+														"title").text();
+												date = $(this).children(
+														'pubDate').text();
+												desc = $(this).children(
+														"description").text();
+												if (desc
+														.match("http://cfile(.*?)\"") !== null) {
+													thumb = desc
+															.match("http://cfile(.*?)\"")[0];
+													thumb = thumb.substring(0,
+															thumb.length - 1);
+												} else {
+													thumb = "";
+												}
+												desc = desc.replace(
+														/(<([^>]+)>)/ig, "");
+												item.link = link;
+												item.thumb = thumb;
+												item.title = title;
+												item.desc = desc;
+												item.date = date;
+												recent_items.push(item);
+											});
+							$("#recentPost li").each(addThumb("C40x40"));
+							initHomeCards(n, l);
+						},
+						error : function() {
+							console.log("RSS loading fail.");
 						}
-						desc = desc.replace(/(<([^>]+)>)/ig, "");
-						item.link = link;
-						item.thumb = thumb;
-						item.title = title;
-						item.desc = desc;
-						item.date = date;
-						recent_items.push(item);
 					});
-					$("#recentPost li").each(addThumb("C40x40"));
-					initHomeCards(n, l); 
-				},
-				error: function() {
-					console.log("RSS loading fail.");
-				}
-			});
 		}
 		function thumbSizing(url, size) {
-				var s = url.replace("image", size);
-				s = s.replace("original", size);
-				return s;
-			}
-			// 최근 글 썸네일 생성
+			var s = url.replace("image", size);
+			s = s.replace("original", size);
+			return s;
+		}
+		// 최근 글 썸네일 생성
 		function addThumb(size) {
-				return function() {
-					var addr = $(this).children("a").attr("href");
-					var r = $(this).find(".thumbnail");
-					for (var i = 0; i < recent_items.length; ++i) {
-						if (addr == decodeURIComponent(recent_items[i].link)) {
-							s = thumbSizing(recent_items[i].thumb, size);
-							r.attr("src", s);
-							r.addClass("byRSS");
-						}
+			return function() {
+				var addr = $(this).children("a").attr("href");
+				var r = $(this).find(".thumbnail");
+				for (var i = 0; i < recent_items.length; ++i) {
+					if (addr == decodeURIComponent(recent_items[i].link)) {
+						s = thumbSizing(recent_items[i].thumb, size);
+						r.attr("src", s);
+						r.addClass("byRSS");
 					}
-				};
-			}
-			// 메인화면 로딩
+				}
+			};
+		}
+		// 메인화면 로딩
 		function initHomeCards(n, l) {
 			if (location.pathname == "/") {
 				m = Math.min(n, recent_items.length);
@@ -77,11 +100,15 @@
 				for (var i = 0; i < m; i++) {
 					var tmpCard = "";
 					tmpCard += '<div class="homeCardWrap col-md-6">';
-					tmpCard += '	<div class="homeCard card ripplelink" onclick="window.open(\'' + recent_items[i].link + '\',\'_self\')">';
-					tmpCard += '		<img src="' + thumbSizing(recent_items[i].thumb, "C220x220") + '"/>';
+					tmpCard += '	<div class="homeCard card ripplelink" onclick="window.open(\''
+							+ recent_items[i].link + '\',\'_self\')">';
+					tmpCard += '		<img src="'
+							+ thumbSizing(recent_items[i].thumb, "C220x220")
+							+ '"/>';
 					tmpCard += '		<div class="headAndDesc">';
 					tmpCard += '			<h3>' + recent_items[i].title + '</h3>';
-					tmpCard += '			<p>' + recent_items[i].desc.substring(0, l) + '...</p>';
+					tmpCard += '			<p>' + recent_items[i].desc.substring(0, l)
+							+ '...</p>';
 					tmpCard += '		</div>';
 					tmpCard += '	</div>';
 					tmpCard += '</div>';
@@ -90,10 +117,127 @@
 				$("#homeCards").show();
 			}
 		}
-		loadViaRSS (10,40)// 홈화면&최근글 썸네일 로딩(RSS갯수,불러올 글자 수)
+		loadViaRSS(10, 40)// 홈화면&최근글 썸네일 로딩(RSS갯수,불러올 글자 수)
 
-	  
-
-  }); // end of document ready
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+// Show Modal when leaving site
+var xValue, yValue;
+var isYIncreasing;
+$(document).mousemove(function(event) {
+
+	var isYIncreasing = yValue > event.pageY;
+	xValue = event.pageX;
+	yValue = event.pageY;
+
+	if (isYIncreasing && event.pageY < 25) {
+
+		if ($.cookie('modal_shown') == null) {
+			$.cookie('modal_shown', 'yes', {
+				expires : 7,
+				path : '/'
+			});
+			$('#modal1').openModal();
+			analytics.track('Modal Shown', {
+				category : 'Newsletter',
+				label : 'New Shops',
+				value : 1
+			});
+		}
+
+	}
+});
+
+var iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false);
+console.log(iOS);
+if (iOS) {
+	$("a").click(function(e) {
+		window.alert = function() {
+		};
+		var link = $(this).data('deeplink');
+		var url = $(this).attr("href");
+		setTimeout(function() {
+			window.location = url;
+		}, 1);
+		window.location = link;
+		// window.location.replace();
+		console.log(link);
+		e.stopPropagation();
+		e.preventDefault();
+	});
+}
+
+if ($.cookie('search-box') == null) {
+	jQuery(".search-box").hide();
+	jQuery(".search-box").slideDown(1000);
+	$.cookie('search-box', 'yes', {
+		expires : 365,
+		path : '/'
+	});
+} else {
+	jQuery(".search-box").show();
+}
+
+// When user starts typing the form
+jQuery('#emailField').one("keypress", function() {
+	analytics.track('Typing', {
+		category : 'Newsletter',
+		label : 'New Shops'
+
+	});
+});
+
+// 이메일 보내는 기능 
+jQuery( ".email-form" ).submit(function( event ) {
+    event.preventDefault();
+    ajaxurl = "/wp-admin/admin-ajax.php";
+    jQuery('.progress').slideDown();
+  
+    var sendIt = jQuery.post( ajaxurl, jQuery( this ).serialize());
+    
+    sendIt.done(function( data ) {
+        console.log(data);
+        jQuery('.email-form').slideUp();
+        jQuery('.success').slideDown();
+        $.cookie('modal_shown', 'yes', { expires: 365, path: '/' });
+        $.cookie('email-cta', 'yes', { expires: 365, path: '/' });
+        // When the form is submitted
+        analytics.track('Subscribed', {
+        category: 'Newsletter',
+        label: 'New Shops',
+        value: 1
+        });
+    });
+});
+
+ jQuery( "#betaForm" ).submit(function( event ) {
+    event.preventDefault();
+    jQuery('.progress').slideDown();
+    ajaxurl = "/wp-admin/admin-ajax.php";
+    var checked = []
+    $("input[name='platform[]']:checked").each(function ()
+    {
+        checked.push($(this).val());
+    });
+    checked = checked.join(", ");
+    
+    var sendIt = jQuery.post( ajaxurl, jQuery( this ).serialize() + "&platforms=" + checked);
+    
+    sendIt.done(function( data ) {
+
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        jQuery('#joinBeta').slideUp();
+        jQuery('#joinedBeta').slideDown();
+
+        $.cookie('modal_shown', 'yes', { expires: 365, path: '/' });
+        $.cookie('email-cta', 'yes', { expires: 365, path: '/' });
+        analytics.track('Subscribed', {
+        category: 'Newsletter',
+        label: 'Beta',
+        value: 1
+        });
+
+    });
+});
