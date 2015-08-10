26a8630d6a0c97dcf51261242243bdc246024075
dusskapark <dusskapark@gmail.com>
2015-08-10 19:30:46 +0900

    비디오 플레이 등 기능 수정
    비디오 플레이 가능하도록 기능 수정

--- js/init.js
+++ js/init.js
@@ -43,11 +43,11 @@ $(document).ready(function() {
                         var card = "<div class='col s12 m12 l4 grid-item " + item.category + "'>" +
                             "<div class='card'>" +
                                 "<div class='card-image waves-effect waves-block waves-light'>" +
-                                    "<a href='#" + item.id + "' class='modal-trigger'><img width='800' height='600' src=' " + item.picture + " ' class='responsive-img'/></a>" +
+                                    "<a href='javascript:$(\"#" + item.id +"\").openModal();'><img width='800' height='600' src=' " + item.picture + " ' class='responsive-img'/></a>" +
                                         "<span class='card-title'>" + item.category + "<i class='material-icons'>play_circle_filled</i></span>" +
                                 "</div>" +
                                 "<div class='card-content'><span class='card-title activator grey-text text-darken-4 truncate'>" + item.name + "</span>" +
-                                    "<p>" + item.description + "</p>" +
+                                    "<a href='#" + item.id + "' class='modal-trigger'><p>" + item.description + "</p></a>" +
                                 "</div>" +
                                 "<div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + item.category + "<i class='material-icons right'>close</i></span>" +
                                 "<ul class='collection'><li class='collection-item avatar'><img src='" + item.profile_image + "' class='circle'>" +
@@ -60,10 +60,14 @@ $(document).ready(function() {
             $("#FeviCard").append(card);
             
                 // 모달 화면을 만든다.
-                var modal = "<div id='" + item.id + "' class='modal'>" +
+                var modal = "<div id='" + item.id + "' class='modal modal-fixed-footer'>" +
                     "<div class='modal-content'>" +
                         "<h4>" + item.name + "</h4>" +
                             "<p>" + item.description + "</p>" +
+                                "<div class='video-container'>" +
+                                    "<video src ='" + item.source + "' controls preload='auto' poster='" + item.picture + "'>" +
+                                    "</video>"+
+                                "</div>"+
                     "</div>" +
                     "<div class='modal-footer'>" +
                         "<a href='#!' class=' modal-action modal-close waves-effect waves-green btn-flat'>Okay</a>" +
@@ -80,6 +84,36 @@ $(document).ready(function() {
             }
         });
     };
-    
-
 });
+
+// var openModal = function(div) {
+    // var id = $(div).data('id');
+    // url_id = 'http://munsangdong.cafe24.com/api/card?id=' + id;
+    // console.log(url_id);
+    // var callApi = function(url_id) {
+        // $.ajax({
+            // type : 'GET',
+            // url : url,
+            // dataType : "json",
+            // success : function() {
+//                 
+            // }
+        // });
+    // }
+//     
+//     
+//     
+    // $("#modal1").openModal();
+// };
+
+// $(#FeviCard).append(function (){
+     // var addmore = "<div class='col s12 m12 l4 grid-item'>" +
+        // "<div class='card small valign-wrapper '>" +
+            // "<h5 class='valign center-align'>" +
+            // "<i class='large material-icons'>add</i>" +
+            // "</h5>" +
+        // "</div>"+
+       // "</div>";
+       // console.log(addmore);
+       // $("#FeviCard").last().append(addmore);
+ // });
