2349ae4554b66f4e9c7502b55801bd89fb6ce4f6
dusskapark <dusskapark@gmail.com>
2015-08-24 01:33:22 +0900

    #11 카드가 열리고 닫히는 액션을 구현
    - 카드가 열리고 닫힐 때 비디오 플레이 조절
    - 카드가 열릴 때, 카드의 크기를 자동으로 조

--- js/init.js
+++ js/init.js
@@ -11,14 +11,38 @@ $(document).ready(function() {
     };
     callApi( url, response_json );
 
+    // 카드를 누르면 카드가 확대된다.
+    $("body").on("click", ".activator", function ( e ){
+      $(this).parents(".grid-item").removeClass("grid-item l4").delay(1000).addClass(".grid expanded l12");
 
-    $( "body" ).on( "click", "i.fullscreen", function( e )  {
-      var dataid = $(this).attr("data-id");
-      var dataid = "#" + dataid;
-      $(dataid).parent().removeClass("grid-item l4").delay(1000).addClass(".grid l12");
-      // $(dataid).children('.card-reveal').removeAttr('style').attr(" style='display: block; transform: translateY(-100%);' ");
+      // 클릭시 비디오가 플레이 된다.
+        var $play = $(this).parents('.card').children('div.card-reveal').children('video').get(0);
+        $play.play();
+
       $('.grid').isotope();
-      });
+    });
+
+    // 닫기를 누르면 카드가 다시 작아진다.
+    $( "body" ).on( "click", "div.card-reveal > span", function( e )  {
+      $(this).parents(".expanded").removeClass(".grid expanded l12").delay(1000).addClass("grid-item l4");
+
+      //비디오는 플레이가 중지된다.
+        var $pause = $(this).parents('.card-reveal').children('video').get(0);
+        $pause.pause();
+
+      $('.grid').isotope();
+    });
+
+    // 공유 버튼을 누르면 모달 팝업이 뜬다.
+    $("body").on ("click", "i.sharing", function(e){
+      var sharing = $(this).attr("data-id");
+      console.log(sharing);
+      $('#modal1').openModal();
+
+    });
+
+
+
 
     function response_json(json) {
         var video_list = json.content;
@@ -35,7 +59,7 @@ $(document).ready(function() {
                         "<span class='card-title activator grey-text text-darken-4 truncate'><img src='" + item.profile_image + "' class='circle smallcircle'> " + item.name + "</span>" +
                             "<p class='activator'>" + item.description + "</p>" +
                     "</div>" +
-                    "<div class='card-reveal' data-id='" + item.id +"'><span class='card-title grey-text text-darken-4'>" + item.category + "<i class='material-icons right'>close</i><i class='material-icons right fullscreen' data-id=" + item.id + "> aspect_ratio</i></span>" +
+                    "<div class='card-reveal' data-id='" + item.id +"'><span class='card-title grey-text text-darken-4'>" + item.category + "<i class='material-icons right close'>close</i><i class='material-icons sharing right' data-id=" + item.id + ">share</i></span>" +
                         "<video controls loop preload='auto' poster='" + item.picture + "' src='" + item.source + "' width='100%' >" +
                         "</video>" +
                         "<ul class='collection'><li class='collection-item avatar'><img src='" + item.profile_image + "' class='circle responsive-img'>" +
